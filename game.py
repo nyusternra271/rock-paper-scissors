5a36b2c9fa015af5f42a61fed851772bb451803b
nyusternra271 <108551939+nyusternra271@users.noreply.github.com>
2022-07-12 19:46:28 -0400

    Renamed the user_input variable to user_selection for consistency

--- game.py
+++ game.py
@@ -8,24 +8,28 @@ print('-------------------')
 # Set up the valid options list for validation and random selection later
 valid_options=["rock", "paper", "scissors"]
 
-# get the user's preference
+# get the user's choice
+user_selection=input("Please choose either 'rock', 'paper', or 'scissors':")
 
-user_input=input("Please choose either 'rock', 'paper', or 'scissors':")
+# get the computer's choice
 computer_selection=random.choice(valid_options)
-if not user_input.lower() in valid_options:
+
+# validate the user's input
+
+if not user_selection.lower() in valid_options:
    print("You entered an invalid choice. Please rerun the program and select 'rock', 'paper', or 'scissors'")
    exit()
 else:
-    print('You chose: '+user_input)
+    print('You chose: '+user_selection)
     print('The computer chose: ' + computer_selection)
     print('-------------------')
 
-    if user_input.lower() == computer_selection:
+    if user_selection.lower() == computer_selection:
         print("The game ended in a draw")
         print('-------------------')
         print('Thanks for playing. Please play again')
         exit()
-    elif (user_input.lower() == "rock" and computer_selection == "scissors") or (user_input.lower() == "paper" and computer_selection == "rock"):
+    elif (user_selection.lower() == "rock" and computer_selection == "scissors") or (user_selection.lower() == "paper" and computer_selection == "rock"):
         print("Congrats! You won!")
         print('-------------------')
         print('Thanks for playing. Please play again')
