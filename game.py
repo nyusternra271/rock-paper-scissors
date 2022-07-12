90fd16670ed6d0cee26490bcdc3b4df713d537d9
nyusternra271 <108551939+nyusternra271@users.noreply.github.com>
2022-07-12 19:55:17 -0400

    Updated game.py
    - Added some more comments
    - Changed the output of user and computer choices to use string interpolation

--- game.py
+++ game.py
@@ -20,21 +20,23 @@ if not user_selection.lower() in valid_options:
    print("You entered an invalid choice. Please rerun the program and select 'rock', 'paper', or 'scissors'")
    exit()
 else:
-    print('You chose: '+user_selection)
-    print('The computer chose: ' + computer_selection)
+    print(f"You chose: {user_selection}")
+    print(f"The computer chose: {computer_selection}")
     print('-------------------')
 
-    if user_selection.lower() == computer_selection:
-        print("The game ended in a draw")
-        print('-------------------')
-        print('Thanks for playing. Please play again')
-        exit()
-    elif (user_selection.lower() == "rock" and computer_selection == "scissors") or (user_selection.lower() == "paper" and computer_selection == "rock"):
-        print("Congrats! You won!")
-        print('-------------------')
-        print('Thanks for playing. Please play again')
-        exit()
-    else:
-        print("Sorry, the computer won this time")
-        print('-------------------')
-        print('Thanks for playing. Please play again')
+# compare the user's choice to the computer's choice and determine the winner according to the rules of the game:
+#
+if user_selection.lower() == computer_selection:
+    print("The game ended in a draw")
+    print('-------------------')
+    print('Thanks for playing. Please play again')
+    exit()
+elif (user_selection.lower() == "rock" and computer_selection == "scissors") or (user_selection.lower() == "paper" and computer_selection == "rock"):
+    print("Congrats! You won!")
+    print('-------------------')
+    print('Thanks for playing. Please play again')
+    exit()
+else:
+    print("Sorry, the computer won this time")
+    print('-------------------')
+    print('Thanks for playing. Please play again')
