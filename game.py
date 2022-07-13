86dba8c19dcc425bf65803a2c0939697a20f4504
nyusternra271 <108551939+nyusternra271@users.noreply.github.com>
2022-07-12 20:45:52 -0400

    Update game.py
    Added more comments

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+# Import the random module which is needed for the computer choice
 import random
 
 # Print the welcome screen
@@ -15,27 +16,28 @@ user_selection=input("Please choose either 'rock', 'paper', or 'scissors':")
 computer_selection=random.choice(valid_options)
 
 # validate the user's input
-
 if not user_selection.lower() in valid_options:
    print("You entered an invalid choice. Please rerun the program and select 'rock', 'paper', or 'scissors'")
    exit()
+# if a valid choice is made, print it out along with the computer's random selection
 else:
     print(f"You chose: {user_selection}")
     print(f"The computer chose: {computer_selection}")
     print('-------------------')
 
 # compare the user's choice to the computer's choice and determine the winner according to the rules of the game:
-#
+# 1. Rock beats Scissors
+# 2. Scissors beats Paper
+# 3. Paper beats Rock
+
 if user_selection.lower() == computer_selection:
     print("The game ended in a draw")
     print('-------------------')
     print('Thanks for playing. Please play again')
-    exit()
-elif (user_selection.lower() == "rock" and computer_selection == "scissors") or (user_selection.lower() == "paper" and computer_selection == "rock"):
-    print("Congrats! You won!")
-    print('-------------------')
-    print('Thanks for playing. Please play again')
-    exit()
+elif (user_selection.lower() == "rock" and computer_selection == "scissors") or (user_selection.lower() == "paper" and computer_selection == "rock") or (user_selection.lower() == "scissors" and computer_selection=="paper"):
+        print("Congrats! You won!")
+        print('-------------------')
+        print('Thanks for playing. Please play again')
 else:
     print("Sorry, the computer won this time")
     print('-------------------')
